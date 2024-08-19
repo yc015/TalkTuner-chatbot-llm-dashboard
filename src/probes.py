3b664cdaba0c69c9d1a10381e26a6d84e83d07dc
Yida Chen <yidachen@g.harvard.edu>
2024-08-19 12:53:01 +0800

    remove redundant definition of training hyperparameters

--- src/probes.py
+++ src/probes.py
@@ -542,19 +542,9 @@ class TwoLayerLinearProbeClassificationMixScaler(nn.Module):
     
 class TrainerConfig:
     # optimization parameters
-    max_epochs = 10
-    batch_size = 64
-    learning_rate = 3e-4
+    learning_rate = 1e-3
     betas = (0.9, 0.95)
-    grad_norm_clip = 1.0
     weight_decay = 0.1 # only applied on matmul weights
-    # learning rate decay params: linear warmup followed by cosine decay to 10% of original
-    lr_decay = False
-    warmup_tokens = 375e6 # these two numbers come from the GPT-3 paper, but may not be good defaults elsewhere
-    final_tokens = 260e9 # (at what point we reach 10% of original LR)
-    # checkpoint settings
-    ckpt_path = None
-    num_workers = 0 # for DataLoader
 
     def __init__(self, **kwargs):
         for k,v in kwargs.items():
